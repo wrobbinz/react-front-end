46f2109c06b2bb5f979aa69751652f785a0fb385
wrobbinz <wrobbinz@gmail.com>
2017-11-11 19:45:37 -0800

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,84 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+const precss = require('precss')
+const autoprefixer = require('autoprefixer')
+
+
+// Paths
+const paths = {
+  DIST: path.resolve(__dirname, 'dist'),
+  SRC: path.resolve(__dirname, 'src'),
+  JS: path.resolve(__dirname, 'src/js'),
+}
+
+// Webpack Configuration
+module.exports = {
+  entry: path.join(paths.JS, 'app.js'),
+  output: {
+    path: paths.DIST,
+    filename: 'app.bundle.js',
+  },
+  // Use html plugin
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(paths.SRC, 'index.html'),
+    }),
+    // new ExtractTextPlugin('style.bundle.css'), // CSS will be extracted to this bundle file
+    new ExtractTextPlugin({
+      filename: 'style.bundle.css',
+      disable: process.env.NODE_ENV === 'development',
+    }),
+    new UglifyJSPlugin(),
+  ],
+  // Loaders configuration
+  // We are telling webpack to use 'babel-loader' for .js and .jsx files
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader',
+        ],
+      },
+      // CSS loader for CSS files
+      // Files will get handled by css loader and then passed to the extract text plugin
+      // which will write it to the file we defined above
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                plugins() {
+                  return [autoprefixer, precss]
+                },
+              },
+            },
+            {
+              loader: 'sass-loader',
+            },
+          ],
+        }),
+      },
+      // File loader for assets (svgs, fonts and videos too)
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+    ],
+  },
+  // Enable importing JS files without specifying their's extenstion
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+}
